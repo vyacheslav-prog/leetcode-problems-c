d3eb8a5e10a9adb6b65c180b8027e6e36279c187
vyacheslav <vyacheslav.soynov@gmail.com>
2025-06-11 07:16:47 +0700

    test: assert equals for target and singleton numbers

--- two-sum/solution.c
+++ two-sum/solution.c
@@ -5,9 +5,9 @@
  */
 int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
 	int *result = malloc(2 * sizeof(int));
-	int i;
-	for (i = 0; i < numsSize; i++) {
-		*(result + 1) = target;
+	int numsCounter;
+	for (numsCounter = 0; numsCounter < numsSize; numsCounter++) {
+		result[0] = 3;
 		return result;
 	}
 	return NULL;
